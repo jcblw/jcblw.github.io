6ba736c8fe3cfb6ade86554acc4a7151a928d31a
Jacob Lowe <jlowe@agentace.com>
2015-09-12 22:45:49 -0700

    more

---
+++ _js/components/emoji.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+import emojify from 'emojify.js'
+
+emojify.setConfig({
+  // use githubs CDN
+  img_dir: 'https://github.global.ssl.fastly.net/images/icons/emoji/'
+})
+
+const Emoji = (props) => {
+  const emojifiedText = emojify.replace(props.content)
+  return (
+    <div className="{props.className}" dangerouslySetInnerHTML={{__html: emojifiedText}}></div>
+  )
+}
+
+export default Emoji
